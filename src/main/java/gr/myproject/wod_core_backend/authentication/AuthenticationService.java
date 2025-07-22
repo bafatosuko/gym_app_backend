00bd52ff6b0108baccba4257f4932dfea696f39c
Th. Konstantinou <bafatosuko@gmail.com>
2025-07-22 18:21:18 +0300

    feat: add authentication package

---
+++ src/main/java/gr/myproject/wod_core_backend/authentication/AuthenticationService.java
@@ -0,0 +1,37 @@
+package gr.myproject.wod_core_backend.authentication;
+
+
+import gr.myproject.wod_core_backend.core.exceptions.AppObjectNotAuthorizedException;
+import gr.myproject.wod_core_backend.dto.AuthenticationRequestDTO;
+import gr.myproject.wod_core_backend.dto.AuthenticationResponseDTO;
+import gr.myproject.wod_core_backend.model.User;
+import gr.myproject.wod_core_backend.repository.UserRepository;
+import gr.myproject.wod_core_backend.security.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+
+    public AuthenticationResponseDTO authenticate(AuthenticationRequestDTO dto)
+            throws AppObjectNotAuthorizedException {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(dto.getUsername(), dto.getPassword()));
+
+        User user = userRepository.findByUsername(authentication.getName())
+                .orElseThrow(() -> new AppObjectNotAuthorizedException("User", "User not authorized"));
+
+        String token = jwtService.generateToken(authentication.getName(), user.getRole().name());
+        return new AuthenticationResponseDTO(user.getFirstname(), user.getLastname(), token);
+    }
+}
---
+++ src/main/java/gr/myproject/wod_core_backend/authentication/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package gr.myproject.wod_core_backend.authentication;
+
+
+import gr.myproject.wod_core_backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+//    @Autowired
+//    public CustomUserDetailsService(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow(()
+                -> new UsernameNotFoundException("User with username " + username + " not found"));
+    }
+}
